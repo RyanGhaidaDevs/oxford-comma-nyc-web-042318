a78e5a20eab0a2df1a9b9cdc3d110b1853ffccba
Ryan Ghaida <ryaboug@gmail.com>
2018-04-08 20:42:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,35 @@
 def oxford_comma(array)
 
-end
+  if array.length == 1
+
+    return array.join(" ")
+
+    end
+
+  if array.length == 2
+
+  return array.join(" and ")
+
+  end
+
+if array.length >= 3
+ arr = []
+ x = []
+
+
+    x <<  array.last
+    array.pop
+
+     x.unshift("and")
+
+     puts x.join(" ")
+
+
+    arr << array
+    arr << x.join(" ")
+
+    arr.join(", ")
+
+ end
+
+ end
